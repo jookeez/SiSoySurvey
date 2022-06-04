eda6ffc053b0ee39d8d70ff228ed8f4e3e8d1984
patooig <patooig@gmail.com>
2022-06-04 00:41:22 -0400

    Cambios y comentarios

--- Pato/app.py
+++ Pato/app.py
@@ -16,14 +16,8 @@ def encuestas_encuestado(mail):
     cur1 = mysql.connection.cursor()
     cur1.execute("SELECT Enc.id_encuesta,Enc.nombre,Enc.descripcion, Enc.estado,Enc.preguntas  FROM(   SELECT E.id_encuesta,E.nombre,E.descripcion, E.estado,E.preguntas FROM Encuestas as E WHERE E.estado='Por realizar') as Enc ,(SELECT r.id_encuesta FROM Responde as r WHERE r.correo = %s ) as Res WHERE Res.id_encuesta=Enc.id_encuesta ",[mail])
     data = cur1.fetchall()
-    for row in data:
-        print(row[0])
-        text = ""
-    
-    p = data
-    
-    return render_template("encuestas.html",data=p)
-#HOLA
+
+    return render_template("encuestas_encuestado.html",data=data)
        
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
