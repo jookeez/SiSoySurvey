c9e4be4856d79244b13027bdde7e949c464b015f
Joaquin Vasquez <developer@jookeez.com>
2022-07-07 18:11:47 -0400

    Upgrade

--- app/app 2.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, redirect
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return redirect('https://encuestas.jookeez.com')
-
-if __name__ == '__main__':
-    app.run(debug=True)
